014deeab4b7d278988fb6d71371d69c56ddd266b
Salvador Bravo <sa.bravo0901@gmail.com>
2025-06-20 16:27:47 -0400

    first commit

---
+++ src/screens/RestaurantsScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export function RestaurantsScreen() {
+  return (
+    <View>
+      <Text>RestaurantsScreen Page</Text>
+    </View>
+  );
+}
